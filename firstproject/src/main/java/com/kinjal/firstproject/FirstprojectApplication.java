3b672c2815c5caea9838e8e0d179076efe584f5c
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-08-23 20:21:28 +0530

    init code

---
+++ firstproject/src/main/java/com/kinjal/firstproject/FirstprojectApplication.java
@@ -0,0 +1,15 @@
+package com.kinjal.firstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstprojectApplication.class, args);
+
+		System.out.println("Welcome to Spring Boot...");
+	}
+
+}

0ce30847e77c39dc2d99b84fabae058f67121330
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-08-28 11:46:05 +0530

    init new project

---
+++ bootjpa/src/main/java/com/kinjal/bootjpa/BootjpaApplication.java
@@ -0,0 +1,13 @@
+package com.kinjal.bootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootjpaApplication.class, args);
+	}
+
+}
---
+++ bootjpa/src/test/java/com/kinjal/bootjpa/BootjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kinjal.bootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
